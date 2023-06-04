ce1412620738b3d1b0f0e31d841f5eda7bd96de0
FF <112873224+stefanaFF@users.noreply.github.com>
2023-06-04 12:07:02 +0300

    Day 5

--- day5.py
+++ day5.py
@@ -72,3 +72,266 @@ insert_point = full_stack.index("Redux")
 sub_lst = ["Python", "SQL"]
 full_stack = full_stack[:insert_point+1]+sub_lst+full_stack[insert_point+1:]
 print(full_stack)
+
+#Exercises: Level 2
+#Point 1
+
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+ascend= sorted(ages)
+descend = sorted(ages, reverse=True)
+print(ascend)
+print(descend)
+print("Min age:", min(ages))
+print("Max age:", max(ages))
+if len(ascend)%2 != 0:
+    print("Median age:", ascend[len(ascend)//2])
+else:
+    mid1 = ascend[len(ascend)//2-1]
+    mid2 = ascend[len(ascend)//2]
+    print("Median age:", int(round((mid1 + mid2)/2,0)))
+
+ages_average = int(round(sum(ages)/len(ages),0))
+print("Averange age:", ages_average)
+
+print("Age range:", max(ages)-min(ages))
+
+if len(ascend)%2 != 0:
+    min_list = ascend[0:len(ascend)//2+1]
+    max_list = ascend[len(ascend)//2:]
+    print("Min list:", min_list)
+    print("Max list:", max_list)
+    min_average = abs(sum(min_list)/len(min_list))
+    max_average = abs(sum(max_list)/len(max_list))
+    print("Min average:", min_average)
+    print("Max average:", max_average)
+else:
+    mid1 = ascend[len(ascend)//2-1]
+    mid2 = ascend[len(ascend)//2]
+    print("Median age:", int(round((mid1 + mid2)/2,0)))
+    min_list = ascend[0:len(ascend)//2]
+    max_list = ascend[len(ascend)//2:]
+    print("Min list:", min_list)
+    print("Max list:", max_list)
+    min_average = abs(sum(min_list)/len(min_list))
+    max_average = abs(sum(max_list)/len(max_list))
+    print("Min average:", min_average)
+    print("Max average:", max_average)
+
+#Point 2
+countries = [
+  'Afghanistan',
+  'Albania',
+  'Algeria',
+  'Andorra',
+  'Angola',
+  'Antigua and Barbuda',
+  'Argentina',
+  'Armenia',
+  'Australia',
+  'Austria',
+  'Azerbaijan',
+  'Bahamas',
+  'Bahrain',
+  'Bangladesh',
+  'Barbados',
+  'Belarus',
+  'Belgium',
+  'Belize',
+  'Benin',
+  'Bhutan',
+  'Bolivia',
+  'Bosnia and Herzegovina',
+  'Botswana',
+  'Brazil',
+  'Brunei',
+  'Bulgaria',
+  'Burkina Faso',
+  'Burundi',
+  'Cambodia',
+  'Cameroon',
+  'Canada',
+  'Cape Verde',
+  'Central African Republic',
+  'Chad',
+  'Chile',
+  'China',
+  'Colombi',
+  'Comoros',
+  'Congo (Brazzaville)',
+  'Congo',
+  'Costa Rica',
+  "Cote d'Ivoire",
+  'Croatia',
+  'Cuba',
+  'Cyprus',
+  'Czech Republic',
+  'Denmark',
+  'Djibouti',
+  'Dominica',
+  'Dominican Republic',
+  'East Timor (Timor Timur)',
+  'Ecuador',
+  'Egypt',
+  'El Salvador',
+  'Equatorial Guinea',
+  'Eritrea',
+  'Estonia',
+  'Ethiopia',
+  'Fiji',
+  'Finland',
+  'France',
+  'Gabon',
+  'Gambia, The',
+  'Georgia',
+  'Germany',
+  'Ghana',
+  'Greece',
+  'Grenada',
+  'Guatemala',
+  'Guinea',
+  'Guinea-Bissau',
+  'Guyana',
+  'Haiti',
+  'Honduras',
+  'Hungary',
+  'Iceland',
+  'India',
+  'Indonesia',
+  'Iran',
+  'Iraq',
+  'Ireland',
+  'Israel',
+  'Italy',
+  'Jamaica',
+  'Japan',
+  'Jordan',
+  'Kazakhstan',
+  'Kenya',
+  'Kiribati',
+  'Korea, North',
+  'Korea, South',
+  'Kuwait',
+  'Kyrgyzstan',
+  'Laos',
+  'Latvia',
+  'Lebanon',
+  'Lesotho',
+  'Liberia',
+  'Libya',
+  'Liechtenstein',
+  'Lithuania',
+  'Luxembourg',
+  'Macedonia',
+  'Madagascar',
+  'Malawi',
+  'Malaysia',
+  'Maldives',
+  'Mali',
+  'Malta',
+  'Marshall Islands',
+  'Mauritania',
+  'Mauritius',
+  'Mexico',
+  'Micronesia',
+  'Moldova',
+  'Monaco',
+  'Mongolia',
+  'Morocco',
+  'Mozambique',
+  'Myanmar',
+  'Namibia',
+  'Nauru',
+  'Nepal',
+  'Netherlands',
+  'New Zealand',
+  'Nicaragua',
+  'Niger',
+  'Nigeria',
+  'Norway',
+  'Oman',
+  'Pakistan',
+  'Palau',
+  'Panama',
+  'Papua New Guinea',
+  'Paraguay',
+  'Peru',
+  'Philippines',
+  'Poland',
+  'Portugal',
+  'Qatar',
+  'Romania',
+  'Russia',
+  'Rwanda',
+  'Saint Kitts and Nevis',
+  'Saint Lucia',
+  'Saint Vincent',
+  'Samoa',
+  'San Marino',
+  'Sao Tome and Principe',
+  'Saudi Arabia',
+  'Senegal',
+  'Serbia and Montenegro',
+  'Seychelles',
+  'Sierra Leone',
+  'Singapore',
+  'Slovakia',
+  'Slovenia',
+  'Solomon Islands',
+  'Somalia',
+  'South Africa',
+  'Spain',
+  'Sri Lanka',
+  'Sudan',
+  'Suriname',
+  'Swaziland',
+  'Sweden',
+  'Switzerland',
+  'Syria',
+  'Taiwan',
+  'Tajikistan',
+  'Tanzania',
+  'Thailand',
+  'Togo',
+  'Tonga',
+  'Trinidad and Tobago',
+  'Tunisia',
+  'Turkey',
+  'Turkmenistan',
+  'Tuvalu',
+  'Uganda',
+  'Ukraine',
+  'United Arab Emirates',
+  'United Kingdom',
+  'United States',
+  'Uruguay',
+  'Uzbekistan',
+  'Vanuatu',
+  'Vatican City',
+  'Venezuela',
+  'Vietnam',
+  'Yemen',
+  'Zambia',
+  'Zimbabwe',
+]
+
+if len(countries)%2 != 0:
+    print("Middle country:", countries[len(countries)//2])
+else:
+    mid_co1 = countries[len(countries)//2-1]
+    mid_co2 = countries[len(countries)//2]
+    print("Middle countries: {} and {}".format(mid_co1, mid_co2))
+
+if len(countries)%2 != 0:
+    print("First half:", countries[0:len(countries)//2])
+    print("Second half:", countries[len(countries)//2+1:])
+else:
+   print("First half:", countries[0:len(countries)//2])
+   print("Second half:", countries[len(countries)//2+1:])
+
+#Point 3
+other_list = ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']
+ch, rs, usa, *scandic_countries = other_list
+print(ch)
+print(rs)
+print(usa)
+print(scandic_countries)
