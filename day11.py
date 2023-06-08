e127cf0aba1b02ed9118a4abda75ac0c27ce1872
FF <112873224+stefanaFF@users.noreply.github.com>
2023-06-08 18:23:07 +0300

    Day 11

--- day11.py
+++ day11.py
@@ -236,3 +236,20 @@ def calculate_std(lst):
     print("The standard deviation of {} is {}.".format(lst,round(std,3)))
     return std
 calculate_std([1, 2, 3, 4, 5, 6])
+
+#Exercises: Level 3
+#Point 1 - Write a function called is_prime, which checks if a number is prime.
+
+
+#Point 2 - Write a functions which checks if all items are unique in the list.
+
+
+#Point 3 - Write a function which checks if all the items of the list are of the same data type.
+
+
+#Point 4 - Write a function which check if provided variable is a valid python variable
+
+
+#Point 5 - Go to the data folder and access the countries-data.py file.
+#Create a function called the most_spoken_languages in the world. It should return 10 or 20 most spoken languages in the world in descending order
+#Create a function called the most_populated_countries. It should return 10 or 20 most populated countries in descending order.
